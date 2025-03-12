dddf16fe4dc78daa67ffc2fe19030b43fdd0d82f
Emilbrain <74027280+Emilbrain@users.noreply.github.com>
2025-03-12 16:32:37 +0300

    Complete profile no adaptive and back-end

---
+++ resources/views/pages/profile__edit.blade.php
@@ -0,0 +1,231 @@
+@extends('layout.page')
+@section('content')
+    <div class="section">
+        <form action="" method="post">
+            @csrf
+            <div class="section body__container">
+                <div class="profile__row">
+                    <div class="profile__column">
+                        <div class="content__border">
+                            <header class="profile-edit__top">
+                                <input type="file" name="avatar" id="avatar">
+                                <div class="profile__avatar">
+                                    <img src="{{asset('images/icon/avatar.png')}}" alt="">
+                                </div>
+                            </header>
+                            <footer class="profile-edit__bottom">
+                                <div class="button profile__saveBtn">
+                                    <a href="">
+                                        Сохранить
+                                    </a>
+                                </div>
+                            </footer>
+                        </div>
+
+                        <div class="profile__edit-links mt30px">
+                            <a href="" class="profile__edit-link active">
+                                Основная информация
+                            </a>
+                            <a href="" class="profile__edit-link">
+                                Контактная информация
+                            </a>
+                            <a href="" class="profile__edit-link">
+                                Профессиональные навыки
+                            </a>
+                            <a href="" class="profile__edit-link">
+                                Приватность/Смена пароля
+                            </a>
+                        </div>
+                    </div>
+                    <div class="profile__column profilePage__container">
+
+                        <div class="content__border">
+                            <div class="profileEdit__top">
+                                <h3>
+                                    Основная информация
+                                </h3>
+                            </div>
+                            <div class="profileEdit__main">
+                                <div class="profileEdit__section mt30px">
+                                    <label for="name">Имя</label>
+                                    <input type="text" name="name" id="name" value="Федотов Евгений Валерьевич">
+                                    <p class="profileEdit__section__subtext">
+                                        Для изменения имени обратитесь к администратору
+                                    </p>
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="birthday">Дата рождения</label>
+                                    <input type="date" name="birthday" id="birthday" value="2025-07-25">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="status">Статус</label>
+                                    <div class="custom-selector">
+                                        <select id="status" name="status">
+                                            <option value="Study">Учусь</option>
+                                            <option value="Work">Работаю</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="Specialization">Специальность/должность</label>
+                                    <input type="text" name="Specialization" id="Specialization" value="веб-разработка">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="Organization">Организация</label>
+                                    <input type="text" name="Organization" id="Organization" value="МЦК-КТИТС">
+                                </div>
+
+
+                                <div class="button profileEdit__saveBtn mt30px">
+                                    <a href="">
+                                        Сохранить
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="content__border mt30px ">
+                            <div class="profileEdit__top">
+                                <h3>
+                                    Контактная информация
+                                </h3>
+                            </div>
+                            <div class="profileEdit__main">
+                                <div class="profileEdit__section mt30px">
+                                    <label for="email">Почта</label>
+                                    <input type="email" name="email" id="email" value="evgen765@gmail.com">
+                                    <p class="profileEdit__section__subtext">
+                                        Для изменения почты обратитесь к администратору
+                                    </p>
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="telegram">Телеграм</label>
+                                    <input type="text" name="telegram" id="telegram" value="2025-07-25">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="City">Город</label>
+                                    <input type="text" name="City" id="City" value="Казань  ">
+                                </div>
+
+
+                                <div class="button profileEdit__saveBtn mt30px">
+                                    <a href="">
+                                        Сохранить
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="content__border mt30px ">
+                            <div class="profileEdit__top">
+                                <h3>
+                                    Профессиональные навыки
+                                </h3>
+                            </div>
+
+                            <div class="profileEdit__main">
+                                <div class="profileEdit__section mt30px">
+                                    <label for="Direction">Направление</label>
+                                    <div class="custom-selector">
+                                        <select id="Direction" name="Direction">
+                                            <option value="web_design">Веб-дизайн</option>
+                                            <option value="front-end">Front-end разработчик</option>
+                                            <option value="back-end">Back-end разработчик</option>
+                                            <option value="full-stack">Full-stack разработчик</option>
+                                        </select>
+                                    </div>
+                                    <p class="profileEdit__section__subtext">
+                                        Для изменения направления обратитесь к администратору
+                                    </p>
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="hard_skills">Hard skills</label>
+                                    <textarea name="hard_skills" id="hard_skills" rows="10">Понимание какой-то технологии, умение пользоваться определенной программой, опыт вождения или знание английского — это все хард-скиллы. Они специфичны для конкретной профессии и обычно остаются неизменными на одной и той же должности в разных компаниях.
+                                    </textarea>
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="soft_skills">Soft skills</label>
+                                    <textarea name="soft_skills" id="soft_skills" rows="5">Например, умение договариваться, стрессоустойчивость, внимательность, самоорганизация или тайм-менеджмент — софт-скиллы.
+                                    </textarea>
+                                </div>
+
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="portfolio">Ссылка на портфолио</label>
+                                    <input type="text" name="portfolio" id="portfolio"
+                                           value="https://www.behance.net/alenakolesni6">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="experience">Опыт работы</label>
+                                    <textarea name="experience" id="experience" rows="5">Фриланс 3 года
+                                    </textarea>
+                                </div>
+
+                                <div class="button profileEdit__saveBtn mt30px">
+                                    <a href="">
+                                        Сохранить
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="content__border mt30px ">
+                            <div class="profileEdit__top">
+                                <h3>
+                                    Приватность
+                                </h3>
+                            </div>
+
+                            <div class="profileEdit__main">
+                                <div class="profileEdit__section mt30px">
+                                    <label for="private">Закрытый профиль</label>
+                                    <input type="checkbox" name="private" id="private" checked>
+                                    <p class="profileEdit__section__subtext">
+                                        В закрытом профиле публично отображается только фотография и имя
+                                    </p>
+                                </div>
+
+                                <h3 class="mt30px">
+                                    Смена пароля
+                                </h3>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="old_password">Старый пароль</label>
+                                    <input type="password" name="old_password" id="old_password">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="password">Новый пароль</label>
+                                    <input type="password" name="password" id="password">
+                                </div>
+
+                                <div class="profileEdit__section mt30px">
+                                    <label for="password-conformation">Повторите новый пароль</label>
+                                    <input type="password" name="password-conformation" id="password">
+                                </div>
+
+                                <div class="button profileEdit__saveBtn mt30px">
+                                    <a href="">
+                                        Сохранить
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/pages/profile__information.blade.php
@@ -0,0 +1,111 @@
+@extends('layout.profile')
+@section('content')
+
+        <header class="profileInformation__top">
+            <h3>
+                Профессиональные навыки
+            </h3>
+        </header>
+        <main class="profileInformation__main">
+            <div class="profileInformation__section mt30px">
+                <h4>
+                    Hard skills
+                </h4>
+                <div class="profileInformation__section__inf mt20px ">
+                    <p>
+                        Понимание какой-то технологии, умение пользоваться определенной программой, опыт вождения или знание английского — это все хард-скиллы. Они специфичны для конкретной профессии и обычно остаются неизменными на одной и той же должности в разных компаниях.
+                    </p>
+                </div>
+                <div class="button profileInformation__section__btn mt20px">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white"/>
+                        </svg>
+
+                        Добавить
+                    </a>
+                </div>
+            </div>
+
+            <div class="profileInformation__section mt30px">
+                <h4>
+                    Soft skills
+                </h4>
+                <div class="profileInformation__section__inf mt20px ">
+                    <p>
+                        Например, умение договариваться, стрессоустойчивость, внимательность, самоорганизация или тайм-менеджмент — софт-скиллы.
+                    </p>
+                </div>
+                <div class="button profileInformation__section__btn mt20px">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white"/>
+                        </svg>
+
+                        Добавить
+                    </a>
+                </div>
+            </div>
+
+            <div class="profileInformation__section mt30px">
+                <h4>
+                    Направление
+                </h4>
+                <div class="profileInformation__section__inf mt20px ">
+                    <div class="profileInformation__section__inf__direction">
+                        Веб-дизайн
+                    </div>
+
+                </div>
+                <div class="button profileInformation__section__btn mt20px">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white"/>
+                        </svg>
+
+                        Добавить
+                    </a>
+                </div>
+            </div>
+
+            <div class="profileInformation__section mt30px">
+                <h4>
+                    Портфолио
+                </h4>
+                <div class="profileInformation__section__inf mt20px ">
+                    <a href="">
+                        https://www.behance.net/alenakolesni6
+                    </a>
+                </div>
+                <div class="button profileInformation__section__btn mt20px">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white"/>
+                        </svg>
+
+                        Добавить
+                    </a>
+                </div>
+            </div>
+            <div class="profileInformation__section mt30px">
+                <h4>
+                    Опыт работы
+                </h4>
+                <div class="profileInformation__section__inf mt20px ">
+                  <p>
+                      Фриланс 3 года
+                  </p>
+                </div>
+                <div class="button profileInformation__section__btn mt20px">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white"/>
+                        </svg>
+
+                        Добавить
+                    </a>
+                </div>
+            </div>
+
+        </main>
+@endsection
